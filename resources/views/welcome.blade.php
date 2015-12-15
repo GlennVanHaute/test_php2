1051c32245b481212e4cf5194296b401bf997e5e
Joris Hens <joris@goodbytes.be>
2015-12-15 18:43:08 +0100

    updated instructions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,12 @@
                 font-family: Helvetica Neue, Helvetica, sans-serif;
             }
 
+            .commit{
+                background-color: yellow;
+                font-size: 10px;
+                font-weight: bold;
+            }
+
             .container {
                 text-align: center;
                 display: table-cell;
@@ -51,7 +57,8 @@
                 <div class="title">Instructies</div>
                 <ul class="instructions">
                     <li>Zorg voor een mini-app met 2 pagina's</li>
-                    <li><code>route <a href="/deadlines/add">/deadlines/add</a></code>: Toevoegen aan databank via formulier:
+                    <li><span class="commit">git commit 1</span> Maak een migratie aan voor de databanktabellen</li>
+                    <li><span class="commit">git commit 2</span> <code>route <a href="/deadlines/add">/deadlines/add</a></code>: Toevoegen aan databank via formulier:
                         <ul>
                             <li>Naam vak (vb: PHP2)</li>
                             <li>Naam opdracht (vb: Dribbble project)</li>
@@ -59,9 +66,10 @@
                             <li>Datum van deadline (vb: 24/12/2015)</li>
                         </ul>
                     </li>
-                    <li><code>route <a href="/deadlines">/deadlines/</a></code> toont u alle deadlines in chronologische volgorde (oudste bovenaan)</li>
-                    <li>Zorg voor een filter (voorbije deadlines / actieve deadlines)</li>
+                    <li><span class="commit">git commit 3</span> <code>route <a href="/deadlines">/deadlines/</a></code> toont u alle deadlines in chronologische volgorde (oudste bovenaan)</li>
+                    <li><span class="commit">git commit 4</span> Zorg voor een filter (voorbije deadlines / actieve deadlines)</li>
                     <li>Zorg voor minstens drie records (1 in verleden en 2 in de toekomst)</li>
+                    <li>Commits die gebeuren na 17u15 maken uw applicatie ongeldig.</li>
                 </ul>
             </div>
 
